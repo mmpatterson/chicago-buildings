6af2f959a5eee1d33ab78180995198b14130083c
Maxwell Patterson <maxwellpatterson@maxwells-air.attlocal.net>
2020-06-20 11:53:53 -0500

    adjusted mapbox links

--- static/js/leaflet1.js
+++ static/js/leaflet1.js
@@ -30,10 +30,10 @@ function genInfo() {
     }
 
     // Create tile layer
-    var lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     maxZoom: 18,
-    id: "mapbox.streets",
+    id: "mapbox/streets-v11",
     accessToken: API_KEY
     });
 
